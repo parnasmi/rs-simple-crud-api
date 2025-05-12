ce3e410b052e89d67bb6ea8e216b35462553fd66
IlhomMax <parnasmi@gmail.com>
2025-05-12 06:38:33 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
