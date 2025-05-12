57124cd03c0e2f3c11bdc25bee53c5144495d2a7
ilkhom_maksadkulov <ilkhom.maksadkulov@opentech.uz>
2025-05-12 12:08:20 +0500

    feat: implement updating user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,66 +1,124 @@
-import { IncomingMessage, ServerResponse } from "http";
-import { users } from "../db/users";
-import { validate as isValidUUID, v4 as uuidv4 } from "uuid";
-import { parseRequestBody } from "../utils/bodyParser";
-import { User } from "../models/user.model";
+import { IncomingMessage, ServerResponse } from 'http';
+import { users } from '../db/users';
+import { validate as isValidUUID, v4 as uuidv4 } from 'uuid';
+import { parseRequestBody } from '../utils/bodyParser';
+import { User } from '../models/user.model';
 
+export const updateUserById = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+  userId: string,
+) => {
+  if (!isValidUUID(userId)) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid UUID' }));
+    return;
+  }
 
-export const createUser = async (req:IncomingMessage, res:ServerResponse) => {
+  const userIndex = users.findIndex((u) => u.id === userId);
+  if (userIndex === -1) {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `User with id ${userId} not found` }));
+    return;
+  }
+
   try {
     const body = await parseRequestBody(req);
-    
-    //TODO: fix typing later
-    const {username, age, hobbies} = body;
+    const { username, age, hobbies } = body;
 
-    if(!username || typeof username !== 'string' || typeof age !== 'number' || !Array.isArray(hobbies) || !hobbies.every((h) => typeof h === 'string')) {
-      res.writeHead(400, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({message: 'Missing or invalid required fields'}));
+    if (
+      !username ||
+      typeof username !== 'string' ||
+      typeof age !== 'number' ||
+      !Array.isArray(hobbies) ||
+      !hobbies.every((h) => typeof h === 'string')
+    ) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({ message: 'Missing or invalid required fields' }),
+      );
+      return;
+    }
+
+    const updatedUser: User = {
+      id: userId,
+      username,
+      age,
+      hobbies,
+    };
+
+    users[userIndex] = updatedUser;
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(updatedUser));
+  } catch (error) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid JSON body' }));
+  }
+};
+
+export const createUser = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const body = await parseRequestBody(req);
+
+    //TODO: fix typing later
+    const { username, age, hobbies } = body;
+
+    if (
+      !username ||
+      typeof username !== 'string' ||
+      typeof age !== 'number' ||
+      !Array.isArray(hobbies) ||
+      !hobbies.every((h) => typeof h === 'string')
+    ) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({ message: 'Missing or invalid required fields' }),
+      );
       return;
     }
 
     const newUser: User = {
-      id:uuidv4(),
+      id: uuidv4(),
       username,
       age,
-      hobbies
-    }
+      hobbies,
+    };
 
     users.push(newUser);
-    
-    res.writeHead(201,{'Content-Type': 'application/json'});
+
+    res.writeHead(201, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(newUser));
-
   } catch (error) {
-    res.writeHead(400, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: 'Invalid JSON body'}));
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid JSON body' }));
   }
-}
-
+};
 
 export const getUserById = (
   req: IncomingMessage,
   res: ServerResponse,
-  userId: string
+  userId: string,
 ) => {
   if (!isValidUUID(userId)) {
-    res.writeHead(400, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Invalid UUID" }));
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid UUID' }));
     return;
   }
 
-  const user = users.find(u => u.id === userId);
+  const user = users.find((u) => u.id === userId);
 
-  if(!user) {
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: 'User not found'}));
+  if (!user) {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User not found' }));
     return;
   }
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(user));
 };
 
 export const getAllUsers = (_req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(users));
 };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,27 +1,40 @@
-import { IncomingMessage, ServerResponse } from "http";
-import { createUser, getAllUsers, getUserById } from "../controllers/users.controller";
+import { IncomingMessage, ServerResponse } from 'http';
+import {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+} from '../controllers/users.controller';
 
 export const handleUserRoutes = (req: IncomingMessage, res: ServerResponse) => {
-  const url = req.url || "";
+  const url = req.url || '';
   const method = req.method;
 
   // GET /api/users
-  if (url === "/api/users" && method === "GET") {
+  if (url === '/api/users' && method === 'GET') {
     return getAllUsers(req, res);
   }
 
-  if(url === '/api/users' && method === 'POST') {
+  // POST /api/users
+  if (url === '/api/users' && method === 'POST') {
     return createUser(req, res);
+  }
+
+  // PUT /api/users/:id
+  if (url.startsWith('/api/users/') && method === 'PUT') {
+    const userId = url.split('/')[3];
+    console.log('userId', { userId, url });
+    return updateUserById(req, res, userId);
   }
 
   // GET /api/users/:id
   const userIdMatch = url.match(/^\/api\/users\/([0-9a-fA-F-]{36})$/);
-  if (userIdMatch && method === "GET") {
+  if (userIdMatch && method === 'GET') {
     const userId = userIdMatch[1];
     return getUserById(req, res, userId);
   }
 
   //If no matching
-  res.writeHead(404, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Route not found" }));
+  res.writeHead(404, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Route not found' }));
 };
